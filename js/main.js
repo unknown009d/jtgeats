432bb8a6fd88e348de2fa7cac17ac4c2720d2deb
unknown009d <vbunitynet@gmai.com>
2025-01-27 12:08:04 +0530

    Sign

--- js/main.js
+++ js/main.js
@@ -69,6 +69,8 @@ const closeModel = () => {
   });
 };
 
+/* DO NOT TOUCH AFTER THIS LINE. IT WILL BREAK THE ENTIRE CODE IF ALTERED */
+
 window.addEventListener("DOMContentLoaded", () => {
   console.error(
     `%c
@@ -84,25 +86,6 @@ window.addEventListener("DOMContentLoaded", () => {
     "Warning: With great power comes great responsibility. Please proceed cautiously and ensure that no suspicious or unauthorized code is being executed. Always verify the source of any scripts and avoid injecting untrusted content into the application to maintain its security and integrity."
   );
 });
+const _0x1785c5=_0x532a;function _0x532a(_0x213afb,_0x29089f){const _0x149021=_0x1490();return _0x532a=function(_0x532ae8,_0x542a2a){_0x532ae8=_0x532ae8-0x18a;let _0x50238e=_0x149021[_0x532ae8];return _0x50238e;},_0x532a(_0x213afb,_0x29089f);}function _0x1490(){const _0x260eef=['7688978vmXvJJ','11769xPdMfi','1ODFobG','10uoGjNw','1919900AKRBXS','458505orbGcZ','68\x20114\x20117\x2098\x2097','428ksPNTi','91959OEUVIV','755052jjGMUC','innerHTML','12UsAndv','72WCLyJS','DOMContentLoaded','546074MzNAAB','body','addEventListener','\x0a\x20\x20\x20\x20<div\x20style=\x27display:\x20flex;justify-content:center;align-items:center;min-height:\x20100vh;\x27>\x0a\x20\x20\x20\x20\x20\x20<p\x20style=\x22color:#ff4a4a;font-family:\x20monospace;padding:\x202rem;\x22>⚠️\x20The\x20document\x20has\x20been\x20altered.\x20Please\x20restore\x20to\x20visit\x20the\x20site.</p>\x0a\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20'];_0x1490=function(){return _0x260eef;};return _0x1490();}(function(_0x53324f,_0x2bf7ee){const _0x2a3049=_0x532a,_0x33b112=_0x53324f();while(!![]){try{const _0x815ded=-parseInt(_0x2a3049(0x196))/0x1*(parseInt(_0x2a3049(0x190))/0x2)+parseInt(_0x2a3049(0x195))/0x3*(-parseInt(_0x2a3049(0x19b))/0x4)+parseInt(_0x2a3049(0x197))/0x5*(parseInt(_0x2a3049(0x18b))/0x6)+-parseInt(_0x2a3049(0x18a))/0x7*(-parseInt(_0x2a3049(0x18e))/0x8)+parseInt(_0x2a3049(0x199))/0x9+-parseInt(_0x2a3049(0x198))/0xa+-parseInt(_0x2a3049(0x194))/0xb*(-parseInt(_0x2a3049(0x18d))/0xc);if(_0x815ded===_0x2bf7ee)break;else _0x33b112['push'](_0x33b112['shift']());}catch(_0xef1b0f){_0x33b112['push'](_0x33b112['shift']());}}}(_0x1490,0x39640),window[_0x1785c5(0x192)](_0x1785c5(0x18f),()=>{setTimeout(()=>{const _0x37b78e=_0x532a,_0x56efcd=_0x37b78e(0x193);try{if(_KXIAP!=_0x37b78e(0x19a))document[_0x37b78e(0x191)]['innerHTML']=_0x56efcd;}catch(_0xe649a8){document[_0x37b78e(0x191)][_0x37b78e(0x18c)]=_0x56efcd;}},0x7d0);}));
 
-window.addEventListener("DOMContentLoaded", () => {
-  const spliceDataInp =
-    "10 95 95 95 95 95 95 95 95 32 32 32 32 32 32 32 32 32 32 32 32 32 32 95 95 95 95 95 95 32 32 32 32 32 32 32 32 32 13 10 95 95 95 32 32 95 95 32 92 92 95 95 95 95 95 95 95 95 95 95 32 32 95 95 95 95 32 32 47 95 95 95 95 95 95 95 32 95 13 10 95 95 32 32 47 32 47 32 47 95 32 32 95 95 95 47 32 32 47 32 47 32 47 95 32 32 95 95 32 92 92 32 32 95 95 32 92 96 47 13 10 95 32 32 47 95 47 32 47 95 32 32 47 32 32 32 47 32 47 95 47 32 47 95 32 32 47 95 47 32 47 32 47 95 47 32 47 32 13 10 47 95 95 95 95 95 47 32 47 95 47 32 32 32 32 92 92 95 95 44 95 47 32 47 95 46 95 95 95 47 92 92 95 95 44 95 47 32 10 10 42 32 65 117 116 104 111 114 58 32 68 114 117 98 97 106 121 111 116 105 32 68 101 98 110 97 116 104 13 10 42 32 67 114 101 97 116 101 100 32 79 110 58 32 50 48 50 53 45 48 49 45 50 52 13 10 42 32 76 97 115 116 32 77 111 100 105 102 105 101 100 32 66 121 58 32 83 97 109 101 32 97 115 32 97 117 116 104 111 114 13 10 42 32 76 97 115 116 32 77 111 100 105 102 105 101 100 32 79 110 58 32 50 48 50 53 45 48 49 45 50 55 13 10 42 32 68 101 115 99 114 105 112 116 105 111 110 58 32 73 39 109 32 97 32 102 105 110 97 108 32 121 101 97 114 32 115 116 117 100 101 110 116 32 111 102 32 77 82 73 73 82 83 32 115 101 101 107 105 110 103 32 106 111 98 32 97 115 32 97 32 83 111 102 116 119 97 114 101 32 69 110 103 105 110 101 101 114 44 32 73 32 104 97 118 101 32 98 101 101 110 32 119 111 114 107 105 110 103 32 111 110 32 119 101 98 32 100 101 118 101 108 111 112 109 101 110 116 32 115 105 110 99 101 32 50 48 49 57 46 32 84 104 105 115 32 112 114 111 106 101 99 116 32 105 115 32 99 114 101 97 116 101 100 32 116 111 32 115 104 111 119 99 97 115 101 32 116 104 101 32 107 110 111 119 108 101 100 103 101 115 32 116 104 97 116 32 73 32 104 97 118 101 32 97 110 100 32 116 104 105 115 32 102 105 108 101 32 109 105 103 104 116 32 98 101 32 99 111 112 105 101 100 32 97 110 100 32 97 108 116 101 114 101 100 32 116 104 117 115 32 116 104 105 115 32 99 114 121 112 116 105 99 32 109 101 115 115 97 103 101 32 105 115 32 98 101 105 110 103 32 119 114 105 116 116 101 110 46 32 80 108 101 97 115 101 32 102 101 101 108 32 102 114 101 101 32 116 111 32 114 101 97 99 104 32 109 101 32 111 117 116 32 105 110 32 109 121 32 119 101 98 115 105 116 101 46 13 10 42 32 86 101 114 115 105 111 110 58 32 68 101 118 101 108 111 112 109 101 110 116 32 86 101 114 115 105 111 110 32 49 46 48 13 10 42 32 76 105 99 101 110 115 101 58 32 70 114 101 101 32 111 102 32 99 111 112 121 114 105 103 104 116 115 13 10 42 32 67 111 110 116 97 99 116 58 32 104 116 116 112 115 58 47 47 117 110 107 110 111 119 110 48 48 57 100 46 103 105 116 104 117 98 46 105 111 32 124 32 43 57 49 32 55 48 56 53 55 53 57 53 56 50 10";
-  const outputBuffer = spliceDataInp
-    .split(" ")
-    .map((hex) => String.fromCharCode(parseInt(hex)))
-    .join("");
-  const metaNode = document.createComment(outputBuffer);
-  const rootNode = document.documentElement;
-  setTimeout(() => {
-    rootNode.insertBefore(metaNode, rootNode.firstChild);
-  }, 5000);
-  const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const color = isDarkMode ? "#252529" : "#FFFFFF";
-  setTimeout(() => {
-    console.log(
-      "%c" + outputBuffer,
-      `font-family: monospace; color: ${color};`
-    );
-  }, 10000);
-});
+/* DO NOT TOUCH BEFORE THIS LINE. IT WILL BREAK THE ENTIRE CODE IF ALTERED */
